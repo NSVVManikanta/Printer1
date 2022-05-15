751a356e60e313110878c6c63feb1e5d4a7e569e
NSVVManikanta <101240994+NSVVManikanta@users.noreply.github.com>
2022-05-15 20:24:43 +0530

    Update printerGroupAssignmentAPIController.js

--- Device/apiControllers/printerGroupAssignmentAPIController.js
+++ Device/apiControllers/printerGroupAssignmentAPIController.js
@@ -1,4 +1,4 @@
-const printerGroupAssignConstroller = require("../controllers/printerGroupAssignmentController")
+const printerGroupAssignController = require("../controllers/printerGroupAssignmentController")
 const status = require("http-status");
 const commonHelper = require('../../Helper');
 const errConstant = require('../../printerGroup/errors/printerGroupErrors')
@@ -7,16 +7,16 @@ const successResponse = commonHelper.successResponse;
 
 
 const assignPrinterGroupReqHandler = async (req, res) => {
-    try {
+  try {
     //   if (!Object.keys(req.body).length) {
     //     throw errConstant.CONST_BAD_REQUEST_ERROR;
     //   }
-      const result = await printerGroupAssignConstroller.assignPrinterGroup(
-        req.body.printerGroupId,
-        req.params.printerId,
-      );
-      res.status(status.CREATED).send(successResponse(result));
-    } catch (err) {
+    const result = await printerGroupAssignController.assignPrinterGroups(
+      req.body.printerGroupId,
+      req.params.printerId,
+    );
+    res.status(status.CREATED).send(successResponse(result));
+  } catch (err) {
     //   switch (err) {
     //     case errConstant.CONST_BAD_REQUEST_ERROR:
     //       res
@@ -29,25 +29,25 @@ const assignPrinterGroupReqHandler = async (req, res) => {
     //         .send(formatResponse(errConstant.CONST_TRIGGERS_NOTARRAY));
     //       break;
     //     default:
-          res
-            .status(status.INTERNAL_SERVER_ERROR)
-            .send(formatResponse(errConstant.CONST_INTERNAL_SERVER_ERROR));
-        //   break;
-      }
-    // }
-  };
+    res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .send(formatResponse(errConstant.CONST_INTERNAL_SERVER_ERROR));
+    //   break;
+  }
+  // }
+};
 
-  const unassignPrinterGroupReqHandler = async (req, res) => {
-    try {
+const unAssignPrinterGroupReqHandler = async (req, res) => {
+  try {
     //   if (!Object.keys(req.body).length) {
     //     throw errConstant.CONST_BAD_REQUEST_ERROR;
     //   }
-      const result = await printerGroupAssignConstroller.unAssignPrinterGroup(
-        req.body.printerGroupId,
-        req.params.printerId,
-      );
-      res.status(status.CREATED).send(successResponse(result));
-    } catch (err) {
+    const result = await printerGroupAssignController.unAssignPrinterGroups(
+      req.body.printerGroupId,
+      req.params.printerId,
+    );
+    res.status(status.CREATED).send(successResponse(result));
+  } catch (err) {
     //   switch (err) {
     //     case errConstant.CONST_BAD_REQUEST_ERROR:
     //       res
@@ -60,12 +60,76 @@ const assignPrinterGroupReqHandler = async (req, res) => {
     //         .send(formatResponse(errConstant.CONST_TRIGGERS_NOTARRAY));
     //       break;
     //     default:
-          res
-            .status(status.INTERNAL_SERVER_ERROR)
-            .send(formatResponse(errConstant.CONST_INTERNAL_SERVER_ERROR));
-        //   break;
-      }
-    // }
-  };
+    res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .send(formatResponse(errConstant.CONST_INTERNAL_SERVER_ERROR));
+    //   break;
+  }
+  // }
+};
 
-  module.exports = {assignPrinterGroupReqHandler,unassignPrinterGroupReqHandler}
+const assignOnePrinterGroupReqHandler = async (req, res) => {
+  try {
+    //   if (!Object.keys(req.body).length) {
+    //     throw errConstant.CONST_BAD_REQUEST_ERROR;
+    //   }
+    const result = await printerGroupAssignController.assignOnePrinterGroup(
+      req.params,
+    );
+    res.status(status.CREATED).send(successResponse(result));
+  } catch (err) {
+    //   switch (err) {
+    //     case errConstant.CONST_BAD_REQUEST_ERROR:
+    //       res
+    //         .status(status.BAD_REQUEST)
+    //         .send(formatResponse(errConstant.CONST_BAD_REQUEST_ERROR));
+    //       break;
+    //     case errConstant.CONST_TRIGGERS_NOTARRAY:
+    //       res
+    //         .status(status.BAD_REQUEST)
+    //         .send(formatResponse(errConstant.CONST_TRIGGERS_NOTARRAY));
+    //       break;
+    //     default:
+    res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .send(formatResponse(errConstant.CONST_INTERNAL_SERVER_ERROR));
+    //   break;
+  }
+  // }
+};
+
+const unAssignOnePrinterGroupReqHandler = async (req, res) => {
+  try {
+    //   if (!Object.keys(req.body).length) {
+    //     throw errConstant.CONST_BAD_REQUEST_ERROR;
+    //   }
+    const result = await printerGroupAssignController.unAssignOnePrinterGroup(
+      req.params,
+    );
+    res.status(status.CREATED).send(successResponse(result));
+  } catch (err) {
+    //   switch (err) {
+    //     case errConstant.CONST_BAD_REQUEST_ERROR:
+    //       res
+    //         .status(status.BAD_REQUEST)
+    //         .send(formatResponse(errConstant.CONST_BAD_REQUEST_ERROR));
+    //       break;
+    //     case errConstant.CONST_TRIGGERS_NOTARRAY:
+    //       res
+    //         .status(status.BAD_REQUEST)
+    //         .send(formatResponse(errConstant.CONST_TRIGGERS_NOTARRAY));
+    //       break;
+    //     default:
+    res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .send(formatResponse(errConstant.CONST_INTERNAL_SERVER_ERROR));
+    //   break;
+  }
+  // }
+};
+
+
+
+
+
+module.exports = { assignPrinterGroupReqHandler, unAssignPrinterGroupReqHandler, assignOnePrinterGroupReqHandler, unAssignOnePrinterGroupReqHandler };
