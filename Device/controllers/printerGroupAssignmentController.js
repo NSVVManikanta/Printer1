257a06133dfe50f3ae423378dce88dfb715961d0
NSVVManikanta <101240994+NSVVManikanta@users.noreply.github.com>
2022-05-15 19:08:43 +0530

    Update printerGroupAssignmentController.js

--- Device/controllers/printerGroupAssignmentController.js
+++ Device/controllers/printerGroupAssignmentController.js
@@ -1,58 +1,96 @@
 const Model = require('../../util/database');
 
 
-const assignPrinterGroup = async (PrinterGroupId,printerId) => {
+const assignPrinterGroups = async (printerGroupArr, printerId) => {
     try {
-        //   if (!Array.isArray(Assign)) {
-        //     throw errConstant.CONST_TRIGGERS_NOTARRAY;
-        //   } else {
-        const findPrinterGroups = await Model.PrinterGroupAssignment.findAll({where: {printerId:printerId}});
-        const printerGroupArr = findPrinterGroups.map((id)=>{
+        const findPrinterGroups = await Model.PrinterGroupAssignment.findAll({ where: { printerId: printerId } });
+        const printerGroupDetails = findPrinterGroups.map((id) => {
             return id.printerGroupId;
         })
-        console.log(printerGroupArr);
-        const arr1=[];
-        const notMatch =  PrinterGroupId.map((pgId)=>{
-            const arr = printerGroupArr.includes(pgId);
-            if(!arr){
-               arr1.push(pgId);
+        console.log( printerGroupDetails);
+        const arr = [];
+        const printerGroupsCompare = printerGroupArr.map((printerGroupId) => {
+            const printerGroups = printerGroupDetails.includes(printerGroupId);
+            if (!printerGroups) {
+                arr.push(printerGroupId);
             }
         })
-        // const printerGroups = PrinterGroupId.every(pg=>!printerGroupArr.includes(pg));
-        console.log(notMatch);
-        const assignmentArrObj = notMatch.map((assign) => {
+        // const printerGroups = printerGroupArr.every(pg=>!printerGroupDetails.includes(pg));
+        console.log(printerGroupsCompare);
+        if(arr.length){
+        const assignmentArrObj = printerGroupsCompare.map((assign) => {
             assign.printerId = printerId;
             assign.assignedBy = printerId;
             return assign;
         });
-       console.log(assignmentArrObj);
+        console.log(assignmentArrObj);
         const printerGroupAssignment = await Model.PrinterGroupAssignment.bulkCreate(assignmentArrObj);
-        //   }
         return printerGroupAssignment;
+    }
+    if(!arr.length){
+        return [{id:printerGroupArr}]
+    }
     } catch (err) {
         console.log(err);
         throw err;
     }
 };
 
-const unAssignPrinterGroup = async (printerGroupId,printerId) => {
+const unAssignPrinterGroups = async (printerGroupId, printerId) => {
     try {
         // const toBeDeletedIds = printerGroupId.map(unAssign => {
         //     unAssign.printerGroupId
         // });
         await Model.PrinterGroupAssignment.destroy({
             where: {
-                [ Model.Op.and]: [
-                    {printerGroupId: printerGroupId},
-                    {printerId:printerId}
-            ]
+                [Model.Op.and]: [
+                    { printerGroupId: printerGroupId },
+                    { printerId: printerId }
+                ]
             }
         });
-        return  [{id:printerGroupId}];
+        return [{ id: printerGroupId }];
     } catch (err) {
         console.log(err);
         throw err;
     }
 };
 
-module.exports = { assignPrinterGroup, unAssignPrinterGroup }
+const assignOnePrinterGroup = async (id) => {
+    try {
+        const printerGroupAssignment = await Model.PrinterGroupAssignment.create({
+            printerGroupId: id.printerGroupId,
+            printerId: id.printerId,
+            assignedBy: id.printerId,
+        });
+        return {
+        id: printerGroupAssignment.id,
+        printerGroupId: printerGroupAssignment.PrinterGroupId,
+        printerId: printerGroupAssignment.printerId,
+        assignedAt: printerGroupAssignment.assignedAt,
+        assignedBy: printerGroupAssignment.assignedBy,
+        }
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+};
+
+const unAssignOnePrinterGroup = async (printerGroupId, printerId) => {
+    try {
+        await Model.PrinterGroupAssignment.destroy({
+            where: {
+                [Model.Op.and]: [
+                    { printerGroupId: printerGroupId },
+                    { printerId: printerId }
+                ]
+            }
+        });
+        return { id: printerGroupId };
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+};
+
+module.exports = { assignPrinterGroups, unAssignPrinterGroups, assignOnePrinterGroup, unAssignOnePrinterGroup }
